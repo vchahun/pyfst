6c16dab9f069adb00591cbf3a0aa7080ff55b2c9
Victor Chahuneau <vchahune@cs.cmu.edu>
2012-08-16 16:34:41 -0400

    Fix gcc optimization level

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ ext_modules = [
     Extension(name='fst',
         sources=['fst.cpp'],
         include_dirs=['.'],
-        libraries=['z', 'fst', 'fstscript'], 
-        extra_compile_args=['-O3'])
+        libraries=['z', 'fst', 'fstscript'],
+        extra_compile_args=['-O2'])
 ]
 
 setup(
